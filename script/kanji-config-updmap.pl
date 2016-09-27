af53fc09a28a251ffccc5dbc362026c802427810
KUROKI Yusuke <kuroky@users.sourceforge.jp>
2016-09-27 16:49:22 +0900

    Enable to guess the version of Windows in Yu fonts

--- script/kanji-config-updmap.pl
+++ script/kanji-config-updmap.pl
@@ -75,7 +75,8 @@ my %representatives = (
   "ipa"           => "ipam.ttf",
   "ipaex"         => "ipaexm.ttf",
   "ms"            => "msgothic.ttc",
-  "yu-win"        => "yumin.ttf",
+  "yu-win"        => "yugothic.ttf",
+  "yu-win10"      => "YuGothM.ttc",
   "yu-osx"        => "YuMin-Medium.otf",
 );
 my %available;
