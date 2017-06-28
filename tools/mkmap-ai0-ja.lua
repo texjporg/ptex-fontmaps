98675942520cf213c0a1d6c76154317394a51c46
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2017-06-28 22:19:30 +0900

    Add tools/mkmap-ai0-*.lua (currently noto-otc only)

---
+++ tools/mkmap-ai0-ja.lua
@@ -0,0 +1,134 @@
+#! /usr/bin/env texlua
+
+-- Adobe-Identity0 なフォントのマップを作る．unicode 専用．
+-- これは up(La)TeX + dvipdfmx (TeX Live 2017) が必須．
+-- つまり，p(La)TeX は不可．dvips も不可．cid も不可．
+local foundry = {
+   ['noto-otc']   = {
+      ml=':0:NotoSerifCJK-Light.ttc',
+      mr=':0:NotoSerifCJK-Regular.ttc',
+      mb=':0:NotoSerifCJK-Bold.ttc',
+      gr=':0:NotoSansCJK-Regular.ttc',
+      gru=':0:NotoSansCJK-Regular.ttc',
+      gb=':0:NotoSansCJK-Bold.ttc',
+      ge=':0:NotoSansCJK-Black.ttc',
+      mgr=':0:NotoSansCJK-Black.ttc',
+      {''},
+   },
+}
+
+-- '#' は 'h', 'v' に置換される
+-- '@' は jaEmbed の値に置換される
+local maps = {
+   ['uptex-@'] = {   -- upTeX 90JIS
+      {'urml',    '-l jp90',      'mr'},
+      {'urmlv',   '-w 1 -l jp90', 'mr'},
+      {'ugbm',    '-l jp90',      'gru'},
+      {'ugbmv',   '-w 1 -l jp90', 'gru'},
+      {'uprml-#', '# -l jp90',    'mr'},
+      {'upgbm-#', '# -l jp90',    'gru'},
+      {'uprml-hq','-l fwid',      'mr'},
+      {'upgbm-hq','-l fwid',      'gru'},
+   },
+   ['uptex-@-04'] = { -- upTeX JIS04
+      {'urml',    '-l jp04',      'mrn'},
+      {'urmlv',   '-w 1 -l jp04', 'mrn'},
+      {'ugbm',    '-l jp04',      'grun'},
+      {'ugbmv',   '-w 1 -l jp04', 'grun'},
+      {'uprml-#', '# -l jp04',    'mrn'},
+      {'upgbm-#', '# -l jp04',    'grun'},
+      {'uprml-hq','-l fwid',      'mrn'},
+      {'upgbm-hq','-l fwid',      'grun'},
+   },
+   ['otf-@'] = {
+      '% Unicode 90JIS',
+      {'otf-ujml-#', '# -l jp90', 'ml'},
+      {'otf-ujmr-#', '# -l jp90', 'mr'},
+      {'otf-ujmb-#', '# -l jp90', 'mb'},
+      {'otf-ujgr-#', '# -l jp90', 'gr'},
+      {'otf-ujgb-#', '# -l jp90', 'gb'},
+      {'otf-ujge-#', '# -l jp90', 'ge'},
+      {'otf-ujmgr-#','# -l jp90', 'mgr'},
+      '% Unicode JIS04',
+      {'otf-ujmln-#', '# -l jp04', 'mln'},
+      {'otf-ujmrn-#', '# -l jp04', 'mrn'},
+      {'otf-ujmbn-#', '# -l jp04', 'mbn'},
+      {'otf-ujgrn-#', '# -l jp04', 'grn'},
+      {'otf-ujgbn-#', '# -l jp04', 'gbn'},
+      {'otf-ujgen-#', '# -l jp04', 'gen'},
+      {'otf-ujmgrn-#','# -l jp04', 'mgrn'},
+   },
+   ['otf-up-@'] = {
+      '% TEXT, 90JIS',
+      {'uphminl-#',  '# -l jp90', 'ml'},
+      {'uphminr-#',  '# -l jp90', 'mr'},
+      {'uphminb-#',  '# -l jp90', 'mb'},
+      {'uphgothr-#', '# -l jp90', 'gr'},
+      {'uphgothb-#', '# -l jp90', 'gb'},
+      {'uphgotheb-#','# -l jp90', 'ge'},
+      {'uphmgothr-#','# -l jp90', 'mgr'},
+      '% TEXT, JIS04',
+      {'uphminln-#',  '# -l jp04', 'mln'},
+      {'uphminrn-#',  '# -l jp04', 'mrn'},
+      {'uphminbn-#',  '# -l jp04', 'mbn'},
+      {'uphgothrn-#', '# -l jp04', 'grn'},
+      {'uphgothbn-#', '# -l jp04', 'gbn'},
+      {'uphgothebn-#','# -l jp04', 'gen'},
+      {'uphmgothrn-#','# -l jp04', 'mgrn'},
+   },
+}
+
+local jis2004_flag = 'n'
+local gsub = string.gsub
+
+function string.explode(s, sep)
+   local t = {}
+   sep = sep or '\n'
+   string.gsub(s, "([^"..sep.."]*)"..sep, function(c) t[#t+1]=c end)
+   return t
+end
+
+local function make_one_line(o, fd, s)
+   if type(o) == 'string' then
+      return '\n' .. o .. '\n'
+   else
+      local fx = foundry[fd]
+      local fn = fx[o[3]]
+      if string.match(o[1], '#') then -- 'H', 'V' 一括出力
+	 return gsub(o[1], '#', 'h') .. '\t' .. "unicode" .. '\t' .. fn .. ' ' .. gsub(o[2], '# ', '') .. '\n'
+          .. gsub(o[1], '#', 'v') .. '\t' .. "unicode" .. '\t' .. fn .. ' ' .. gsub(o[2], '# ', '-w 1 ') .. '\n'
+      else
+	 return o[1] .. '\t' .. "unicode" .. '\t' .. fn .. ' ' .. o[2] .. '\n'
+      end
+   end
+end
+
+for fd, v1 in pairs(foundry) do
+   -- separate でないときは mln などのデータベースを省略してあるので ml などからコピー
+   if not foundry[fd].separate then
+      foundry[fd].mln = foundry[fd].ml
+      foundry[fd].mrn = foundry[fd].mr
+      foundry[fd].mbn = foundry[fd].mb
+      foundry[fd].grn = foundry[fd].gr
+      foundry[fd].grun = foundry[fd].gru
+      foundry[fd].gbn = foundry[fd].gb
+      foundry[fd].gen = foundry[fd].ge
+      foundry[fd].mgrn = foundry[fd].mgr
+   end
+   for _,s in pairs(v1[1]) do
+      local dirname = fd
+      print('jaEmbed: ' .. dirname)
+      -- Linux しか想定していない
+      os.execute('mkdir ' .. dirname .. ' &>/dev/null')
+      for mnx, mcont in pairs(maps) do
+	 if not string.match(mnx, '-04') or not foundry[fd].noncid or foundry[fd].separate then
+	    local mapbase = gsub(mnx, '@', dirname)
+	    local f = io.open(dirname .. '/' .. mapbase .. '.map', 'w+')
+	    for _,x in ipairs(mcont) do
+	       f:write(make_one_line(x, fd, s))
+	    end
+	    f:close()
+	 end
+      end
+   end
+end
---
+++ tools/mkmap-ai0-ko.lua
@@ -0,0 +1,70 @@
+#! /usr/bin/env texlua
+
+-- Adobe-Identity0 なフォントのマップを作る．unicode 専用．
+-- これは up(La)TeX + dvipdfmx (TeX Live 2017) が必須．
+-- つまり，p(La)TeX は不可．dvips も不可．cid も不可．
+local foundry = {
+   ['noto-otc']   = {
+      mr=':1:NotoSerifCJK-Regular.ttc',
+      gr=':1:NotoSansCJK-Regular.ttc',
+      {''},
+   },
+}
+
+-- '#' は 'h', 'v' に置換される
+-- '@' は koEmbed の値に置換される
+local maps = {
+   ['uptex-ko-@'] = {
+      {'uphysmjm-#', '#', 'mr'},
+      {'uphygt-#', '#', 'gr'},
+   },
+   ['otf-ko-@'] = {
+      '% Unicode',
+      {'otf-ukmr-#', '#', 'mr'},
+      {'otf-ukgr-#', '#', 'gr'},
+   },
+}
+
+local jis2004_flag = 'n'
+local gsub = string.gsub
+
+function string.explode(s, sep)
+   local t = {}
+   sep = sep or '\n'
+   string.gsub(s, "([^"..sep.."]*)"..sep, function(c) t[#t+1]=c end)
+   return t
+end
+
+local function make_one_line(o, fd, s)
+   if type(o) == 'string' then
+      return '\n' .. o .. '\n'
+   else
+      local fx = foundry[fd]
+      local fn = fx[o[3]]
+      if string.match(o[1], '#') then -- 'H', 'V' 一括出力
+	 return gsub(o[1], '#', 'h') .. '\t' .. "unicode" .. '\t' .. fn .. ' ' .. gsub(o[2], '#', '') .. '\n'
+          .. gsub(o[1], '#', 'v') .. '\t' .. "unicode" .. '\t' .. fn .. ' ' .. gsub(o[2], '#', '-w 1 ') .. '\n'
+      else
+	 return o[1] .. '\t' .. "unicode" .. '\t' .. fn .. ' ' .. o[2] .. '\n'
+      end
+   end
+end
+
+for fd, v1 in pairs(foundry) do
+   for _,s in pairs(v1[1]) do
+      local dirname = fd
+      print('koEmbed: ' .. dirname)
+      -- Linux しか想定していない
+      os.execute('mkdir ' .. dirname .. ' &>/dev/null')
+      for mnx, mcont in pairs(maps) do
+	 if not string.match(mnx, '-04') or not foundry[fd].noncid then
+	    local mapbase = gsub(mnx, '@', dirname)
+	    local f = io.open(dirname .. '/' .. mapbase .. '.map', 'w+')
+	    for _,x in ipairs(mcont) do
+	       f:write(make_one_line(x, fd, s))
+	    end
+	    f:close()
+	 end
+      end
+   end
+end
